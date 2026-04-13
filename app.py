b438d9f770e50d93116184ffe0c1c97d47d824c0
O-TYAN64 <vrchat0064@gmail.com>
2026-04-13 09:20:20 +0900

    OCR設定を環境変数から取得するように変更し、app.pyでのインポートを修正

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import time
 
-import vrc_yolo_capture
+import vrc_person_speaker_listener
 import talk_ai 
 
 
 
 if __name__ == "__main__":
     print("🚀 main.py 起動")   
-    vrc_yolo_capture.capture()
+    #vrc_person_speaker_listener.run()
     # 少し待つ（マイク・デバイス安定）
     time.sleep(1)
     talk_ai.start_talk_ai()
